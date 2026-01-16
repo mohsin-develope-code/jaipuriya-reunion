8e9900af4bba48b12698e6a75747ef4fe2b04dc2
Mohammad Mohsin <mosn0078600@gmail.com>
2026-01-16 22:38:51 +0530

    UI Update

--- main.js
+++ main.js
@@ -396,7 +396,7 @@ async function paymentRazorpay() {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        amount: 1, //totalPayPrice,
+        amount: 1, //totalPayPrice, final production time will open
         formData: formData,
       }),
     });
